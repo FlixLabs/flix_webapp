e88050b8806c6ad36828d4fd246376b9184b452b
Renaud Baivier <renaudbaivier@gmail.com>
2025-03-01 15:15:24 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    radarr: {
+        baseURL: "http://ip:7878",
+        apiKey: "La_cle_API"
+    },
+    sonarr: {
+        baseURL: "http://ip:8989",
+        apiKey: "La_cle_API"
+    }
+};
